5e9e7d0e520ccd013c8bf664a1d784e920abb523
Youssef Elassri <youssef3.elassri@gmail.com>
2024-08-11 01:02:52 +0100

    export collections

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -1,1 +1,27 @@
 package exporter
+
+import (
+	"encoding/json"
+	"fmt"
+	"http-to-postman/pkg/models"
+	"os"
+)
+
+func ExportPostmanCollections(collections models.PostmanCollection, filename string) error {
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(collections)
+	if err != nil {
+		return fmt.Errorf("failed to encode collections: %v", err)
+	}
+
+	fmt.Printf("Successfully wrote collection to %s\n", filename)
+	return nil
+}
