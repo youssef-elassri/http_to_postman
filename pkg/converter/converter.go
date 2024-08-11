d31c458db016861c5e2e59e8641f582faf544252
Youssef Elassri <youssef3.elassri@gmail.com>
2024-08-11 01:02:12 +0100

    add conversion logic

--- pkg/converter/converter.go
+++ pkg/converter/converter.go
@@ -1,1 +1,57 @@
 package converter
+
+import (
+	"http-to-postman/pkg/models"
+)
+
+func ConvertToPostmanCollection(requests []models.Request) models.PostmanCollection {
+	items := httpRequestsToPostmanItems(requests)
+
+	return models.PostmanCollection{
+		Info: models.CollectionInfo{
+			Name:   "Converted Collection",
+			Schema: "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+		},
+		Item: items,
+	}
+}
+
+func httpRequestsToPostmanItems(requests []models.Request) []models.PostmanItem {
+	var items []models.PostmanItem
+	for _, request := range requests {
+		items = append(items, models.PostmanItem{
+			Name:    request.Description,
+			Request: httpRequestToPostmanRequest(request),
+		})
+	}
+
+	return items
+}
+
+func httpRequestToPostmanRequest(request models.Request) models.PostmanRequest {
+
+	postmanUrl := models.PostmanURL{
+		Raw: request.Url,
+	}
+
+	var postmanHeaders []models.PostmanHeader
+	for _, header := range request.Headers {
+		postmanHeaders = append(postmanHeaders, models.PostmanHeader{
+			Key:   header.Key,
+			Value: header.Value,
+		})
+	}
+
+	postmanBody := models.PostmanBody{
+		Mode: "raw", // TODO: Support other modes
+		Raw:  request.Body,
+	}
+
+	return models.PostmanRequest{
+		Method: request.Method,
+		Url:    postmanUrl,
+		Header: postmanHeaders,
+		Body:   postmanBody,
+	}
+
+}
